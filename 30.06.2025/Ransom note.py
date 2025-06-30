a2ffe76aad6c9f26f0b99cb1e7f4d0ad45f8d667
Dharsinijayaprakash <151325936+Dharsinijayaprakash@users.noreply.github.com>
2025-06-30 18:26:35 +0530

    Create Ransom note.py

---
+++ 30.06.2025/Ransom note.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canConstruct(self, ransomNote, magazine):
+        from collections import Counter
+        
+        ransom_count = Counter(ransomNote)
+        magazine_count = Counter(magazine)
+        
+        for char, count in ransom_count.items():
+            if magazine_count[char] < count:
+                return False
+        
+        return True
